4b117dc4501778dcbe744bf72f7874039f14ef3f
Julio Serrata <58531265+juliospau@users.noreply.github.com>
2023-03-02 22:45:42 +0100

    Update xss-scanner.py

--- xss-scanner.py
+++ xss-scanner.py
@@ -12,6 +12,9 @@ CYAN = Fore.CYAN
 
 class scanner():
 
+    def __init__(self, payload):
+        self.payload = payload
+
     def crawl(self, url):
 
         self.url = url
@@ -36,11 +39,15 @@ class scanner():
 
         # Se escanea la URL introducida y se pasa a un segundo nivel a partir de los enlaces encontrados
 
-        links = self.crawl(input("\n-> Introduce la URL a escanear: "))
-
+        inpLink = input("\n-> Introduce la URL a escanear: ")
+        if inpLink[-1] != "/": inpLink += "/";
+        
+        links = self.crawl(inpLink)
+        
         print (f"{GREEN}[+] Escaneando la URL ...")
         
         level2 = []
+
 
         for l2 in links:
             if l2 not in level2:
@@ -48,68 +55,77 @@ class scanner():
 
 
         return level2
-  
-    def tryPayload(self, targetLinks, payload):
 
-        print (f"{GREEN}[+] Escaneando los formularios y sus campos ...")
-        
-        self.targetLinks = targetLinks
-        self.payload = payload
+    def xssForm(self, filterForm, link):
+
+        self.link = link
+        self.filterForm = filterForm
         triggeredForms = []
 
-        # Por cada enlace se escanean los formularios y se prueba a enviar el payload establecido como valor de un campo tipo texto
-        
-        for i in self.targetLinks:
-            response = requests.get(i)
-            parsedHTML = bs(response.content, features="html.parser")
-            filterForm = parsedHTML.findAll("form")
+        for form in self.filterForm:
 
-            for form in filterForm:
-
-                if form not in triggeredForms:
-                    triggeredForms.append(form)
+            if form not in triggeredForms:
+                triggeredForms.append(form)
                 
 
-                    action = form.get("action")
-                    postURL = uritools.urijoin(i, action)
+                action = form.get("action")
+                postURL = uritools.urijoin(self.link, action)
 
-                    inputList = form.findAll("input")
+                inputList = form.findAll("input")
 
-
-                    postData = {}
+                postData = {}
             
-                    for inputF in inputList:
+                method = form.get("method")
+                    
+                for inputF in inputList:
 
-                        inputName = inputF.get("name")
-                        inputType = inputF.get("type")
-                        inputValue = inputF.get("value")
+                    inputName = inputF.get("name")
+                    inputType = inputF.get("type")
+                    inputValue = inputF.get("value")
 
-                        if inputType == "text":
-                            inputValue = self.payload
+                    if inputType == "text":
+                        inputValue = self.payload
 
-                            postData.update({inputF.get("name") : inputF.get("value")})
+                        postData.update({inputF.get("name") : inputValue})
                 
-                        if inputType == "submit":
-                            postData.update({inputF.get("name") : inputF.get("value")})
+                    if inputType == "submit":
+                        postData.update({inputF.get("name") : inputF.get("value")})
 
 
                     # Al finalizar la recolección de atributos y modificación de valores, se procede a enviar cada formulario
 
+                if method == "post":
                     result = requests.post(postURL, data=postData)
                     parsedResponse = bs(result.content, features="html.parser")
 
-                    # Si se encuentra el payload se indica como XSS reflejado
+                # Si se encuentra el payload se indica como XSS reflejado
 
                     xss = re.findall(self.payload, str(parsedResponse))
-                    if xss is not None:
-                        print (f"{RED}[+] Posible XSS Reflejado en {CYAN}{i} {RED}en el formulario:{YELLOW}\n\n{form}\n")
+                
+                    for vuln in xss:
+                        print (f"{RED}[+] Posible XSS Reflejado en {CYAN}{self.link} {RED}en el formulario:{YELLOW}\n\n{form}\n")
                         print (f"{RED}[+] Razón: se ha podido reflejar el payload {CYAN}{self.payload} {RED} en el campo de entrada de texto\n")
 
                 else:
                     pass
 
 
+    def tryPayload(self, targetLinks):
 
-scann = scanner()
+        print (f"{GREEN}[+] Escaneando los formularios y sus campos ...")
+        
+        self.targetLinks = targetLinks
+        # Por cada enlace se escanean los formularios y se prueba a enviar el payload establecido como valor de un campo tipo texto
+        
+        for link in self.targetLinks:
+            response = requests.get(link)
+            parsedHTML = bs(response.content, features="html.parser")
+            filterForm = parsedHTML.findAll("form")
+        
+            self.xssForm(filterForm, link)
+
+
+scann = scanner("<h1>POC23</h1>")
 targetLinks = scann.crawlSecLevel()
-scann.tryPayload(targetLinks, "<h1>POC23</h1>")
+scann.tryPayload(targetLinks)
+
